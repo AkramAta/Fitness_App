4ccd0b316689ff165e04b822768740e4134b6ec0
AkramAta <akramatm012@gmail.com>
2024-09-09 05:07:18 +0300

    firebase Setup

--- fitness_trainer/lib/main.dart
+++ fitness_trainer/lib/main.dart
@@ -2,8 +2,18 @@ import 'package:finalproject/pages/Tell_us.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
     debugShowCheckedModeBanner: false,
     home: TellUs(),
-  ));
+  );
+  }
 }
