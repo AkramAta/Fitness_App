cbbecb3293b4c5e6fc63ba9a4a6dec1601a30b50
mohamed-yasser <48107732+yasser5555@users.noreply.github.com>
2024-09-09 07:14:56 +0300

    Add files via upload

---
+++ fitness_trainer/Component/SizedBox_for Gapping.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget Sized_Gap({
+  required double Height,
+  double Width = 0,
+}) {
+  return SizedBox(
+    height: Height,
+    width: Width,
+  );
+}
